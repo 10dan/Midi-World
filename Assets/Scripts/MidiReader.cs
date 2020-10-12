70c1045fb35982b126b0d41a23aa096c9048f0a0
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-10-12 15:22:57 +0100

    Best shit i've ever made
    Perfect place to build levels from.

--- Assets/Scripts/MidiReader.cs
+++ Assets/Scripts/MidiReader.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Diagnostics;
 using UnityEngine.Rendering.PostProcessing;
+using System;
 
 public class MidiReader : MonoBehaviour {
 
@@ -14,20 +15,13 @@ public class MidiReader : MonoBehaviour {
 
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip song;
-
-
-    [SerializeField] GameObject[] objects;
-    bool[] objectsActive;
-
-
-    [SerializeField] float shrinkRate;
-
     List<MidiFile> midis = new List<MidiFile>();
+    List<List<int>> uniqueNotes = new List<List<int>>();
+    List<MusicalResponder> responders;
+    public bool[][] objectsActive;
+    Thread musicThread;
 
     private void Awake() {
-        //Set the active notes list to same length as the corresponding number of objects.
-        objectsActive = new bool[objects.Length];
-
         //Create the midi files and store them in list called midis.
         string path = "Assets/Resources/Midis/";
         DirectoryInfo d = new DirectoryInfo(path);
@@ -38,60 +32,66 @@ public class MidiReader : MonoBehaviour {
             }
         }
 
+        //Analize the number of tracks, and number of notes, update in uniqueNotes[][].
+        AnalizeMidi();
 
-    }
+        //Set the active notes list to same length as the corresponding number of objects.
+        objectsActive = new bool[uniqueNotes.Count][];
+        for (int i = 0; i < uniqueNotes.Count; i++) {
+            objectsActive[i] = new bool[uniqueNotes[i].Count];
+        }
 
-    private void Start() {
-        Invoke("StartSong", 1f);
-    }
-    void StartSong() {
-        Thread t = new Thread(new ThreadStart(ThreadMidi));
-        t.Start();
+        musicThread = new Thread(new ThreadStart(ThreadMidi));
+        musicThread.Start();
         audioSource.PlayOneShot(song);
     }
-
-    int boxSize = 3;
-    private void Update() {
-        for (int i = 0; i < objectsActive.Length; i++) {
-            if (objectsActive[i] == true) {
-                objects[i].transform.localScale = new Vector3(boxSize, boxSize, boxSize);
-                objectsActive[i] = false;
-            }
-            if (objects[i].transform.localScale.x > shrinkRate) {
-                objects[i].transform.localScale -= new Vector3(shrinkRate, shrinkRate, shrinkRate);
-            }
+    private void FixedUpdate() {
+        //If the music ends.
+        if (!(audioSource.isPlaying)) {
+            //Reset event pointers and tick counter t.
+            audioSource.PlayOneShot(song);
+            t = 0;
+            currentEvents  = new int[midis.Count];
         }
     }
 
-
+    private void AnalizeMidi() {
+        for (int i = 0; i < midis.Count; i++) {
+            MidiTrack track = midis[i].Tracks[0];
+            uniqueNotes.Add(new List<int>());
+            foreach (MidiEvent e in track.MidiEvents) {
+                if (e.MidiEventType == MidiEventType.NoteOn) {
+                    if (uniqueNotes[i].Contains(e.Note)) {
+                        //Do nothing as note as been accounted for.
+                    } else {
+                        uniqueNotes[i].Add(e.Note);
+                    }
+                }
+            }
+        }
+    }
+    int t;
+    int[] currentEvents;
     public void ThreadMidi() {
-        int t = 0; //Time in ticks
-        int[] currentEvents = new int[midis.Count];
+        t = 0; //Time in ticks
+        currentEvents = new int[midis.Count];
         MidiEvent e;
         while (true) {
+
             //Go through all midi tracks
             for (int i = 0; i < midis.Count; i++) {
+
                 //Set e to the next scheduled event. (track[0] is default, ableton exports that way)
                 e = midis[i].Tracks[0].MidiEvents[currentEvents[i]];
+
                 //While t == e.Time check if its a note event, if not move on. (While loop incase 2 events are at same time)
                 while (t == e.Time) {
                     if (e.MidiEventType == MidiEventType.NoteOn) {
-                        //print(e.Note);
-                        if (e.Note == 36) {
-                            objectsActive[0] = true;
-                        }
-                        if (e.Note == 38) {
-                            objectsActive[2] = true;
-                        }
-                        if (e.Note == 40) {
-                            objectsActive[3] = true;
-                        }
-                        if (e.Note == 41) {
-                            objectsActive[1] = true;
-                        }
+                        ActivateNote(i, e.Note);
                     }
+
                     //If we get to the end of the list of events reset the counter.
-                    if (currentEvents[i] < midis[i].Tracks[0].MidiEvents.Count-1) {
+                    if (currentEvents[i] < midis[i].Tracks[0].MidiEvents.Count - 1) {
                         currentEvents[i]++;
                     } else {
                         currentEvents[i] = 0;
@@ -108,6 +108,14 @@ public class MidiReader : MonoBehaviour {
             }
             //Wait for one tick.
             PauseThreadForSeconds(timeBetweenTicks);
+        }
+    }
+
+    private void ActivateNote(int track, int note) {
+        for (int j = 0; j < uniqueNotes[track].Count; j++) {
+            if (note == uniqueNotes[track][j]) {
+                objectsActive[track][j] = true;
+            }
         }
     }
 
---
+++ Assets/Scripts/MusicalResponder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicalResponder : MonoBehaviour {
+    [SerializeField] int track = 0; //The midi track this responder belongs to.
+    [SerializeField] int id = 0; //The identification number.
+
+    enum Behavior {
+        Explode, Contract, Color
+    }
+    [SerializeField] Behavior behavior;
+    [SerializeField] float shrinkRate=0.02f;
+    private MidiReader midiReader;
+
+    //Info needed for scaling.
+    private Vector3 originalSize;
+
+    private void Start() {
+        originalSize = transform.localScale;
+        midiReader = GameObject.FindObjectOfType<MidiReader>();
+    }
+    private void FixedUpdate() {
+        if(midiReader.objectsActive[track][id] == true) {
+            transform.localScale = new Vector3(2, 2, 2);
+            midiReader.objectsActive[track][id] = false;
+        }
+        if(transform.localScale.x > shrinkRate) {
+            transform.localScale -= new Vector3(shrinkRate, shrinkRate, shrinkRate);
+        }
+    }
+}
